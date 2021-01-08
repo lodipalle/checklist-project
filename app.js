4e7af3bff4d1a2e23c092359ebf555fec8d65596
lodi palle <lodipalle@gmail.com>
2021-01-08 15:31:38 +1100

    new changes

--- app.js
+++ app.js
@@ -17,15 +17,15 @@ form.addEventListener('submit', addItem);
 // ****** FUNCTIONS **********
 function addItem(e) {
   e.preventDefault();
-  console.log();
   const value = grocery.value;
   const id = new Date().getTime().toString();
-  if(value !== "" && editFlag === false) {
+  if(value && !editFlag) {
     console.log("add item to the list");
-  } else if (value !== "" && editFlag === true) {
+  } else if (value && editFlag) {
     console.log("editing");
   } else {
-    console.log("empty value");
+    alert.textContent = "empty value";
+    alert.classList.add("alet-danger");
   }
 }
 // ****** LOCAL STORAGE **********
