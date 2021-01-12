11653cc0e6f9ead623eb963a99ccf51d3178d078
lodi palle <lodipalle@gmail.com>
2021-01-12 12:22:17 +1100

    added changes

--- app.js
+++ app.js
@@ -43,6 +43,10 @@ function addItem(e) {
     displayAlert("item added to the list", "success");
     // show container
     container.classList.add("show-container");
+    // add to local storage
+    addToLocalStorage(id,value);
+    // set back to default
+    setBackToDefault()
   } else if (value && editFlag) {
     console.log("editing");
   } else {
@@ -60,6 +64,12 @@ function displayAlert(text, action) {
     alert.classList.remove(`alet-${action}`);  
   }, 1000);
 }
+// Set back to default
+function setBackToDefault() {
+  console.log("set back to default");
+}
 // ****** LOCAL STORAGE **********
-
+function addToLocalStorage(id, value) {
+  console.log("added to local storage");
+}
 // ****** SETUP ITEMS **********
